a044eafa9240d1fcdc8e51484cb430d35cea89c8
kareldewit1 <k.p.dewit@students.uu.nl>
2015-04-30 13:06:42 +0000

    algoritm more efficient

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -1,22 +1,32 @@
 import sys
 import timeit
+import math
 
+# initialisatie
 T1 = timeit.default_timer()
-
 N = int(sys.argv[1])
+amount = 0
 output = sys.argv[2]
 
-lijst = list(range(2, N))
+# aanmaken van de lijsten
+priemgetallen = list(range(2, N))
+delers = list(range(3,int(math.sqrt(N)),2))
+delers.insert(0, 2)
 
-for mini in lijst:
-    for getal in lijst:
-        if (getal%mini ==0 and getal!=mini):
-            lijst.remove(getal)
+#Bepalen van de priemgetallen
+for deler in delers:
+    for getal in priemgetallen[(deler**2)-2:N-1]:
+        if (getal%deler ==0 and getal!=deler):
+            priemgetallen[getal-2]=0
 
+# Schrijven naar outputnaam
 primes = open(output,'w')
-for getal in lijst:
-    primes.write(str(getal) + "\n")
-    
+for getal in priemgetallen:
+    if(getal != 0):
+        amount += 1
+        primes.write(str(getal) + "\n")
+
+# Einde tijdregistratie    
 T2 = timeit.default_timer()
 
-print('Found ' + str(len(lijst)) + ' Prime numbers smaller than ' +str(N) + ' in ' + str(T2-T1) + ' sec.')
+print('Found ' + str(amount) + ' Prime numbers smaller than ' +str(N) + ' in ' + str(T2-T1) + ' sec.')
